140f3a8fd7ed7f8280d2e5a319889d6b42a23870
thomshaw92 <thomasbasilshaw@gmail.com>
2020-01-10 16:16:33 +1000

    Fixes to the readme etc
    Some more fixes to the documentation. Still has bug from last commit.

--- backup_files_to_rdm.sh
+++ backup_files_to_rdm.sh
@@ -3,18 +3,19 @@
 
 #this script will go into a specified project (dumu_dir)
 #then search that directory for folders that contain .IMA files (dicoms)
-#this needs to be done in levels unless there is a quicker way of doing it. 
-#these folders will be archived (tar) and sent to RDM 
-#then the entire folder is sent to RDM.
+#each folder that contains IMAs will be archived
+#the entire folder is sent to RDM excluding the folders that contian .IMA files (using the --exclude flag of tar)
+#then the archived folders are deleted. 
 
 dumu_dir=~/test_dumu_dir
 #/data/dumu/barth/7TShare/Data/3_studies/7Tea
 rdm_dir=~/test_rdm_dir
 #/winmounts/uqtshaw/data.cai.uq.edu.au/SEVENTEA-Q0757/7Tea_backup_from_DUMU/
 
-#find all the folders that have IMA files in them
+#find all the folders that have IMA files in them 
 #find ${dumu_dir} -name '*.IMA' -printf '%h\n' | sort -u
 #loop over the directories by making it into an array
+
 cd $dumu_dir
 i=0
 while read line
@@ -38,9 +39,14 @@ exclude_options=()
 for x in "${array[@]}"; do
   exclude_options+=(--exclude="$x")
 done
+
 cd $dumu_dir
 tar -czvf $rdm_dir/transferred_from_dumu.tar.gz "${exclude_options[@]}" ./
 
 #rm all the tar files in the dumu dir to save space
-
-
+include_options=()
+for y in "${array[@]}"; do
+    include_options+=("${y}.tgz ")
+done
+		      
+rm -r ${include_options} 
