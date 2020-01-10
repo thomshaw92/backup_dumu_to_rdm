68a334376240c441a9fc2445baf591749c142200
thomshaw92 <thomasbasilshaw@gmail.com>
2020-01-10 16:04:54 +1000

    Fixed issues with tar-ing files
    Made a few changes with the arrays so that it would correctly archive files
    Bug still needs to be fixed where, if the folder contains another folder with an IMA image,
    the folder is archived in its present format in the directory in which it was archived
    or something....

--- backup_files_to_rdm.sh
+++ backup_files_to_rdm.sh
@@ -7,30 +7,39 @@
 #these folders will be archived (tar) and sent to RDM 
 #then the entire folder is sent to RDM.
 
-dumu_dir=/data/dumu/barth/7TShare/Data/3_studies/7Tea
-rdm_dir=/winmounts/uqtshaw/data.cai.uq.edu.au/SEVENTEA-Q0757/7Tea_backup_from_DUMU/
+dumu_dir=~/test_dumu_dir
+#/data/dumu/barth/7TShare/Data/3_studies/7Tea
+rdm_dir=~/test_rdm_dir
+#/winmounts/uqtshaw/data.cai.uq.edu.au/SEVENTEA-Q0757/7Tea_backup_from_DUMU/
 
 #find all the folders that have IMA files in them
 #find ${dumu_dir} -name '*.IMA' -printf '%h\n' | sort -u
 #loop over the directories by making it into an array
-
+cd $dumu_dir
 i=0
 while read line
 do
     array[ $i ]="$line"        
     (( i++ ))
-done < <(find ${dumu_dir} -name '*.IMA' -printf '%h\n' | sort -u)
+done < <(find . -name '*.IMA' -printf '%h\n' | sort -u)
 
 #loop through the array $i number of times and tar each folder as you do it. 
 j=0
-while ((j < $i)) ;do
-    tar cvzf ${array[$j]}.tar.gz -C ${j} .
-    cd ${dumu_dir}
+while ((j < $i)) ; do
+    cd $dumu_dir
+    #cd ${array[$j]}
+    tar cfz ${array[$j]}.tgz --files-from <(printf "%s\n" "${array[$j]}")
+    #tar -cvzf ${array[$j]}.tar.gz ${array[$j]} 
     let j++
 done
 
 #send the whole directory to UQRDM but excluding all the directories in the array
-tar  --exclude-tag-under="*IMA"
+exclude_options=()
+for x in "${array[@]}"; do
+  exclude_options+=(--exclude="$x")
+done
+cd $dumu_dir
+tar -czvf $rdm_dir/transferred_from_dumu.tar.gz "${exclude_options[@]}" ./
 
 #rm all the tar files in the dumu dir to save space
 
