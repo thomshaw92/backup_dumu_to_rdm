b5e3c8f18870a995380ed363e14b1c1b41d49bd3
thomshaw92 <thomasbasilshaw@gmail.com>
2020-01-20 17:30:58 +1000

    Fixes to the code
    previous version did not play nicely with folders within folders with IMAs so fixed that in a new loop
    also fixed the tar command to be less annoying and more straightforward

--- backup_files_to_rdm.sh
+++ backup_files_to_rdm.sh
@@ -6,11 +6,9 @@
 #each folder that contains IMAs will be archived
 #the entire folder is sent to RDM excluding the folders that contian .IMA files (using the --exclude flag of tar)
 #then the archived folders are deleted. 
-
-dumu_dir=~/test_dumu_dir
-#/data/dumu/barth/7TShare/Data/3_studies/7Tea
-rdm_dir=~/test_rdm_dir
-#/winmounts/uqtshaw/data.cai.uq.edu.au/SEVENTEA-Q0757/7Tea_backup_from_DUMU/
+#USE FULL PATH 
+dumu_dir=/data/dumu/barth/7TShare/Data/3_studies/7Tea
+rdm_dir=/winmounts/uqtshaw/data.cai.uq.edu.au/SEVENTEA-Q0757/7Tea_backup_from_DUMU/
 
 #find all the folders that have IMA files in them 
 #find ${dumu_dir} -name '*.IMA' -printf '%h\n' | sort -u
@@ -18,19 +16,22 @@ rdm_dir=~/test_rdm_dir
 
 cd $dumu_dir
 i=0
-while read line
-do
-    array[ $i ]="$line"        
-    (( i++ ))
-done < <(find . -name '*.IMA' -printf '%h\n' | sort -u)
+shopt -s dotglob
+shopt -s nullglob
+array=(`find . -name '*.IMA' -printf '%h\n' | sort -u`)
+for dir in "${array[@]}" ; do
+    echo "$dir"
+    (( i++))
+done
 
 #loop through the array $i number of times and tar each folder as you do it. 
 j=0
 while ((j < $i)) ; do
     cd $dumu_dir
-    #cd ${array[$j]}
-    tar cfz ${array[$j]}.tgz --files-from <(printf "%s\n" "${array[$j]}")
-    #tar -cvzf ${array[$j]}.tar.gz ${array[$j]} 
+    cd ${array[$j]}
+    folder=${PWD##*/}
+    cd ../
+    tar cfz ${folder}.tar.gz ${folder}/*
     let j++
 done
 
@@ -44,9 +45,8 @@ cd $dumu_dir
 tar -czvf $rdm_dir/transferred_from_dumu.tar.gz "${exclude_options[@]}" ./
 
 #rm all the tar files in the dumu dir to save space
-include_options=()
+
 for y in "${array[@]}"; do
-    include_options+=("${y}.tgz ")
+    rm -r ${y}.tar.gz  
 done
-		      
-rm -r ${include_options} 
+
